65d0ea212531db794b06d1b1ef8bfc8238e8bce3
Kyriakos Psarakis <kpsarakis94@gmail.com>
2020-08-20 21:23:37 +0300

    add mstterial UI html elements

--- client/src/components/Forms/Input/Input.js
+++ client/src/components/Forms/Input/Input.js
@@ -2,39 +2,68 @@ import React from 'react'
 
 import classes from './Input.css'
 import Aux from '../../../hoc/Aux'
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
 
 const input = (props) => {
-    let inputElement = null;
+    let inputElement;
+
+    const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+    const mat_classes = useStyles();
+
 
     switch (props.elementType){
         case('input'):
-            inputElement = <input className={classes.InputElement} {...props.config} value={props.value} />;
+            inputElement = <TextField  label={props.name} variant="outlined" {...props.config} value={props.value}
+                                       onChange={props.changed} />;
             break;
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.config} value={props.value} />;
+            inputElement = <textarea className={classes.InputElement} {...props.config} value={props.value}
+                                     onChange={props.changed} />;
             break;
         case('select'):
-            inputElement = <select
-                className={classes.InputElement}
-                value={props.value}>
-                {props.config.options.map(option => (
-                    <option key={option.value} value={option.value}>
-                        {option.displayValue}
-                    </option>
-                ))}
-            </select>
+            inputElement =
+                <FormControl className={mat_classes.formControl}>
+                    <InputLabel>{props.name}</InputLabel>
+                    <Select
+                            value={props.value}
+                            onChange={props.changed}>
+                        {props.config.options.map(option => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.displayValue}
+                            </MenuItem>))}
+                    </Select>
+                </FormControl>
             break;
         case('checkbox'):
+            inputElement = <FormControlLabel
+                control={<Checkbox {...props.config} onChange={props.changed} name={props.name} color="primary"/>}
+                label={props.name} />
+            break;
+        case('range'):
             inputElement =
                 <Aux>
-                    <label>
-                        Default Algorithm Parameters
-                    </label>
-                    <input id="cb" type="checkbox"/>
+                    <label>{props.name}</label>
+                    <input type="range" {...props.config} onChange={props.changed}/>
+                    <label>Value: {props.value}</label>
                 </Aux>
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.config} value={props.value} />;
+            inputElement = null
             break;
     }
     return(
--- client/src/containers/Matcher/JobRequest/JobRequest.js
+++ client/src/containers/Matcher/JobRequest/JobRequest.js
@@ -3,55 +3,199 @@ import React, { Component } from 'react'
 import classes from './JobRequest.css'
 // import axios from '../../../../axios-schema-matching-job'
 import Input from '../../../components/Forms/Input/Input'
+import Button from '../../../components/UI/Button/Button'
+
 
 class JobRequest extends Component {
 
     state = {
+
         jobForm: {
             dbName: {
+                name:'Database Name',
                 elementType: 'input',
                 elementConfig: {
                     type: 'text',
                     placeholder: 'Database Name'
                 },
-                value: ''
+                value: '',
+                show: true
             },
             tableName: {
+                name:'Table Name',
                 elementType: 'input',
                 elementConfig: {
                     type: 'text',
                     placeholder: 'Table Name'
                 },
-                value: ''
+                value: '',
+                show: true
             },
             algorithm: {
+                name:'Select algorithm',
                 elementType: 'select',
                 elementConfig: {
                     options: [
                         {value: 'coma', displayValue: 'COMA'},
-                        {value: 'cupid', displayValue: 'CUPID'}
+                        {value: 'cupid', displayValue: 'CUPID'},
+                        {value: 'distributionBased', displayValue: 'Distribution Based'},
+                        {value: 'jaccardLeven', displayValue: 'Jaccard Levenshtein'},
+                        {value: 'similarityFlooding', displayValue: 'Similarity Flooding'}
                         ]
                 },
-                value: ''
+                value: 'coma',
+                show: true
             },
             defaultAlgoParams: {
+                name:'Default Algorithm Parameters',
                 elementType: 'checkbox',
                 elementConfig:{
                     type: 'checkbox',
-                    checked: true,
+                    defaultChecked: true,
                     name: 'Default Parameters'
                 },
-                value: ''
+                value: true,
+                show: true
             },
+            // START OF PARAMS
+            // COMA params
+            coma_strategy: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'COMA_OPT', displayValue: 'COMA_OPT'},
+                        {value: 'COMA_OPT_INST', displayValue: 'COMA_OPT_INST'}
+                        ]
+                },
+                value: '',
+                show: false
+            },
+            coma_max_n: {
+                name: 'max_n',
+                elementType: 'input',
+                elementConfig: {
+                    type: 'number',
+                    placeholder: 'max_n'
+                },
+                value: 0,
+                show: false
+            },
+            // CUPID params
+            cupid_leaf_w_struct: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.2,
+                show: false
+            },
+            cupid_w_struct: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.2,
+                show: false
+            },
+            cupid_th_accept: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.7,
+                show: false
+            },
+            cupid_th_high: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.6,
+                show: false
+            },
+            cupid_th_low: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.35,
+                show: false
+            },
+            cupid_th_ns: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.7,
+                show: false
+            },
+            // Distribution Based params
+            distributionBased_th1: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.15,
+                show: false
+            },
+            distributionBased_th2: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.15,
+                show: false
+            },
+            distributionBased_quantiles: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 1,
+                    max: 1024,
+                    step: 1
+                },
+                value: 256,
+                show: false
+            },
+            // Jaccard Leven params
+            jaccardLeven_th_leven: {
+                elementType: 'range',
+                elementConfig : {
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.05
+                },
+                value: 0.8,
+                show: false
+            },
+            // END OF PARAMS
             maxNumberOfMatches: {
+                name:'Max number of matches',
                 elementType: 'input',
                 elementConfig: {
                     type: 'number',
                     placeholder: 'Max number of matches'
                 },
-                value: ''
+                value: '',
+                show: true
             },
             mode: {
+                name: 'Schema Matching mode',
                 elementType: 'select',
                 elementConfig: {
                     options: [
@@ -60,19 +204,71 @@ class JobRequest extends Component {
                         {value: 'specifyDB', displayValue: 'Specify Database'}
                         ]
                 },
-                value: ''
+                value: 'holistic',
+                show: true
+            },
+            otherDB: {
+                name: 'Other Database Name',
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Database Name'
+                },
+                value: '',
+                show: false
             }
         },
         loading: false
     }
 
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedJobForm = {
+            ...this.state.jobForm
+        };
+        const updatedJobElement = {
+            ...updatedJobForm[inputIdentifier]
+        };
+        if(inputIdentifier === 'defaultAlgoParams') {
+            updatedJobElement.value = !updatedJobElement.value;
+            this.toggle_show_dynamic_component(updatedJobForm, updatedJobForm.algorithm.value)
+        }else if(inputIdentifier === 'algorithm'){
+            if(!updatedJobForm.defaultAlgoParams.value){
+                this.toggle_show_dynamic_component(updatedJobForm, updatedJobForm.algorithm.value)
+                this.toggle_show_dynamic_component(updatedJobForm, event.target.value)
+            }
+            updatedJobElement.value = event.target.value;
+        }else{
+            updatedJobElement.value = event.target.value;
+            if(inputIdentifier === 'mode' && event.target.value === 'specifyDB'){
+                updatedJobForm['otherDB'].show = true
+            } else if (inputIdentifier === 'mode' && event.target.value !== 'specifyDB'){
+                updatedJobForm['otherDB'].show = false
+            }
+        }
+        updatedJobForm[inputIdentifier] = updatedJobElement;
+        this.setState({jobForm: updatedJobForm});
+    }
+
+    toggle_show_dynamic_component = (updatedJobForm, name) => {
+        for (let key in updatedJobForm) {
+            if (updatedJobForm.hasOwnProperty(key)){
+                if (key.startsWith(name)) {
+                    updatedJobForm[key].show = !updatedJobForm[key].show
+                }
+            }
+        }
+    }
+
     render () {
         const formElementsArray = [];
         for (let key in this.state.jobForm){
-            formElementsArray.push({
-                id: key,
-                config: this.state.jobForm[key]
-            })
+            if (this.state.jobForm[key].show){
+                formElementsArray.push({
+                    id: key,
+                    config: this.state.jobForm[key]
+                })
+            }
         }
         return (
            <div className={classes.JobRequest}>
@@ -83,8 +279,11 @@ class JobRequest extends Component {
                             key={formElement.id}
                             elementType={formElement.config.elementType}
                             config={formElement.config.elementConfig}
-                            value={formElement.config.value} />
+                            name={formElement.config.name}
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                     ))}
+                    <Button btnType={"Success"} clicked={this.jobRequestHandler} >Submit</Button>
                 </form>
            </div>
         );
